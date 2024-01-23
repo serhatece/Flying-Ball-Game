cd82eb81787f78683e7cd666dd732b075a087f11
Serhat Ece <134696090+serhatecee@users.noreply.github.com>
2024-01-23 15:24:00 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace Flying_Ball_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.control = new System.Windows.Forms.Button();
+            this.ball = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 464);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(452, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(522, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "3";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Navy;
+            this.label4.Location = new System.Drawing.Point(440, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 444);
+            this.label4.TabIndex = 3;
+            // 
+            // control
+            // 
+            this.control.BackColor = System.Drawing.Color.Aqua;
+            this.control.Location = new System.Drawing.Point(198, 450);
+            this.control.Name = "control";
+            this.control.Size = new System.Drawing.Size(75, 23);
+            this.control.TabIndex = 4;
+            this.control.UseVisualStyleBackColor = false;
+            this.control.Click += new System.EventHandler(this.control_Click);
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Red;
+            this.ball.Location = new System.Drawing.Point(198, 92);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(17, 16);
+            this.ball.TabIndex = 5;
+            this.ball.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(499, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(17, 16);
+            this.button1.TabIndex = 6;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(481, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 21);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Kalan Canlarınız:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(645, 513);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.control);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button control;
+        private System.Windows.Forms.Button ball;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flying_Ball_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int yerX = 5, yerY = 5, can = 3;
+
+        private void CarpmaOlayı()
+        { 
+            // label2 carpma
+            if(ball.Top <= label2.Bottom)
+            {
+                yerY = yerY * -1;
+            }
+
+            // kontrole carpma olayı
+            if(ball.Bottom >= control.Top && ball.Left >= control.Left && ball.Right <= control.Right)
+            {
+                yerY = yerY * -1;
+            }
+
+            //sag kenara carpma olayı
+            else if(ball.Right >= label4.Left)
+            {
+                yerX = yerX * -1;
+            }
+            //sol kenara carpma olayı
+            else if(ball.Left <= label1.Right)
+            {
+                yerX = yerX * -1;
+            }
+        }
+
+        private void YanmaOLayı(object sender, EventArgs e)
+        {
+            if(ball.Top >= label4.Bottom)
+            {
+                if(can > 0)
+                {
+                    timer1.Stop();
+                    can--;
+                    MessageBox.Show("Yandınız kalan can : " + can.ToString());
+                    Form1_Load(sender, e);
+                }
+
+                if (can == 0)
+                {
+                    timer1.Stop();
+                    MessageBox.Show("Game Over ","",MessageBoxButtons.OK);
+                }
+            }
+            label3.Text = can.ToString();
+        }
+
+        private void Topbasa()
+        {
+            ball.Location = new Point(198, 92);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            control.Left = e.X;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Topbasa();
+            timer1.Enabled = true;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void control_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            CarpmaOlayı();
+            YanmaOLayı(sender,e);
+            ball.Location = new Point(ball.Location.X + yerX, ball.Location.Y + yerY);
+        }
+    }
+}
